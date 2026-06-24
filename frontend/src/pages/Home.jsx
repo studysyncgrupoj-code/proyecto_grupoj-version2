2744628e5180d99e7880872241e51ec04119b242
Felipe Rod <luisfelipeespinel7@gmail.com>
2026-06-24 15:52:53 -0500

    I added new information

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <section className="py-32">
 
         <h1 className="text-4xl md:text-6xl font-bold leading-tight">
-          Bienvenido a
+          Bienvenido a nuestra
           <span className="text-cyan-400"> StudySync</span>
         </h1>
 
